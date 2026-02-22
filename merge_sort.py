70c9c51a52068a16b815d23c3f06c92e37e48f1d
shivendra1312 <sharmashivendra206@gmail.com>
2026-02-22 18:19:34 +0530

    Create merge_sort.py

---
+++ merge_sort.py
@@ -0,0 +1,37 @@
+def merge(left, right):
+    i, j = 0, 0
+    result = []
+    n, m = len(left), len(right)
+
+    while i < n and j < m:
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+    while i < n:
+        result.append(left[i])
+        i += 1
+
+    while j < m:
+        result.append(right[j])
+        j += 1
+
+    return result
+
+def merge_sort(arr):
+    if len(arr)<=1:
+        return arr
+    mid = len(arr)//2
+    left_arr = arr[:mid]
+    right_arr = arr[mid:]
+    left = merge_sort(left_arr)
+    right = merge_sort(right_arr)
+    return merge(left,right)
+
+
+a = [2,5,2,6,8,5,3]
+
+print(merge_sort(a))
